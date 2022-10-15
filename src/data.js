a5a6b1d514e9e93432f64d8955746d381ee8eda2
apnan <aishajahan2015@gmail.com>
2022-10-14 14:10:22 -1200

    share code

---
+++ src/data.js
@@ -0,0 +1,84 @@
+const data = { 
+
+    products: [
+        {
+            _id: '1',
+            name: 'Baby girl party dress',
+            catagorey: 'dress',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'Molo',
+            rating:4.5,
+            numReviews:10,
+            description:'high wuality product'
+        },
+        {
+            _id: '2',
+            name: 'Baby girl party dress',
+            catagorey: 'dress',
+            image: '/images/p2.jpg',
+            price: 100,
+            brand: 'Molo',
+            rating:4.5,
+            numReviews:10,
+            description:'high wuality product'
+        },
+        {
+            _id: '3',
+            name: 'Baby girl party dress',
+            catagorey: 'dress',
+            image: '/images/p3.jpg',
+            price: 110,
+            brand: 'Molo',
+            rating:4.5,
+            numReviews:10,
+            description:'high wuality product'
+        },
+        {
+            _id: '4',
+            name: 'Baby girl party dress',
+            catagorey: 'dress',
+            image: '/images/p4.jpg',
+            price: 90,
+            brand: 'Molo',
+            rating:4.5,
+            numReviews:10,
+            description:'high wuality product'
+        },
+        {
+            _id: '5',
+            name: 'Baby girl party dress',
+            catagorey: 'dress',
+            image: '/images/p5.jpg',
+            price: 150,
+            brand: 'Molo',
+            rating:4.5,
+            numReviews:10,
+            description:'high wuality product'
+        },
+        {
+            _id: '6',
+            name: 'Baby girl party dress',
+            catagorey: 'dress',
+            image: '/images/p6.jpg',
+            price: 100,
+            brand: 'Molo',
+            rating:4.5,
+            numReviews:10,
+            description:'high wuality product'
+        },
+        {
+            _id: '7',
+            name: 'Baby girl party dress',
+            catagorey: 'dress',
+            image: '/images/p7.jpg',
+            price: 99,
+            brand: 'Molo',
+            rating:4.5,
+            numReviews:10,
+            description:'high wuality product'
+        },
+        
+    ],
+};
+export default data;
